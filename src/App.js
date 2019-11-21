511c4e4175bce1c8d817569dd9b504ae0ed30ec7
Prince Ajuobi <princekelsey299@gmail.com>
2019-11-21 16:42:12 +0200

    pages, menuItem and directory component setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./Components/homepage/Homepage";
+import Homepage from "./Pages/homepage/Homepage";
 
 function App() {
   return (
---
+++ src/Pages/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../Components/directory/Directory";
+
+import "./homepage.style.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
