1f5b856ecc4d505ebe186b10b051a6f06cdbaa9a
Prince Ajuobi <princekelsey299@gmail.com>
2020-02-23 22:25:42 +0200

    added stripe payment backend

--- client/src/Components/stripe-button/StripeButton.js
+++ client/src/Components/stripe-button/StripeButton.js
@@ -1,14 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 import logo from "../../utils/logo.svg";
 
 const StripeButton = ({ price }) => {
   const stripePrice = price * 100;
-  const publishedKey = "pk_test_6KSmsUgbuD3GDXT582kj7hRT00GfzdL681";
+  const publishedKey = "pk_test_mA6ngPMWTRMHuQ9kRrZEArDR00rsOcY6kH";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "POST",
+      data: {
+        amount: stripePrice,
+        token
+      }
+    })
+      .then(res => {
+        alert("Payment Successful");
+      })
+      .catch(err => {
+        console.log("Payment error:", JSON.parse(err));
+        alert(
+          "There was an issue with payment. Please make sure you use the provided credit card"
+        );
+      });
   };
   return (
     <StripeCheckout
